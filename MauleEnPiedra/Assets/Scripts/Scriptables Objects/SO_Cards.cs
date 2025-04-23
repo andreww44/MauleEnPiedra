9437dbcd0c334641ca9393a6a9c03e2d9ffe9a43
andreww44 <72322317+andreww44@users.noreply.github.com>
2025-04-23 01:39:19 -0400

    Carta

---
+++ MauleEnPiedra/Assets/Scripts/Scriptables Objects/SO_Cards.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SO_Cards : ScriptableObject
+{
+    public new string name;
+    //public
+    public string description;
+    public string region;
+    public int part;
+}
