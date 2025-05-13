2c82557d0266b0aa8ddb79d392ead3499f49c951
andreww44 <72322317+Asheratto@users.noreply.github.com>
2025-05-12 22:17:46 -0400

    Update

--- MauleEnPiedra/Assets/Scripts/Scriptables Objects/SO_Cards.cs
+++ MauleEnPiedra/Assets/Scripts/Scriptables Objects/SO_Cards.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
+
+[CreateAssetMenu(fileName = "Card", menuName = "SO/Card")]
 public class SO_Cards : ScriptableObject
 {
     public new string name;
-    //public
+    public Card type;
     public string description;
     public string region;
     public int part;
+    public Image image;
 }
---
+++ MauleEnPiedra/Assets/Scripts/Scripts/Game/SCR_Rules.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+//Esta clase se encarga de toda la logica de revisar si se cumples las reglas
+
+public static class Scr_Rules
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public static void CompareCards()
+    {
+
+    }
+}
